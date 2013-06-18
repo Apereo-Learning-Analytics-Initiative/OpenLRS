21f1c568978556e4935dbfaf1d38080890c6d2a0
Aaron Zeckoski <azeckoski@gmail.com>
2013-06-18 10:23:26 -0400

    adding in sample services

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -50,7 +50,7 @@ grails.json.legacy.builder = false
 // enabled native2ascii conversion of i18n properties files
 grails.enable.native2ascii = true
 // packages to include in Spring bean scanning
-grails.spring.bean.packages = []
+grails.spring.bean.packages = ["org.apereo.openlrs"]
 // whether to disable processing of multi part requests
 grails.web.disable.multipart=false
 
@@ -89,4 +89,6 @@ log4j = {
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
+
+    info   'org.apereo.openlrs'
 }
--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
 // Place your Spring DSL code here
+//xmlns context:"http://www.springframework.org/schema/context"
+//context.'component-scan'('base-package': "org.apereo.openlrs")
 beans = {
 }
