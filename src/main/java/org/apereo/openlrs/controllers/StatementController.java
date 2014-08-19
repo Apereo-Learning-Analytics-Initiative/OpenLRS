908a62244ca46bc1d48ae219e85d510fc9bec260
Robert E. Long <rlong@unicon.net>
2014-08-19 14:14:47 -0400

    UOLD-12 UOLD-13 UOLD-14 initial statement controller code

---
+++ src/main/java/org/apereo/openlrs/controllers/StatementController.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2014 Unicon (R) Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+package org.apereo.openlrs.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller to handle GET and POST calls
+ * see https://github.com/adlnet/xAPI-Spec/blob/master/xAPI.md#stmtapi
+ * 
+ * @author Robert E. Long (rlong @ unicon.net)
+ *
+ */
+@RestController
+@RequestMapping("/xAPI/statements")
+public class StatementController {
+
+    /**
+     * Get all statements
+     * @return JSON string of all statement objects
+     */
+    @RequestMapping(value={"", "/", "*"}, method=RequestMethod.GET, produces="application/json")
+    public String getAllStatements() {
+        return "{\"GET all statements\": true}";
+    }
+
+    /**
+     * Get a statement for a specified ID
+     * 
+     * @return JSON string of the statement object with the specified ID
+     */
+    @RequestMapping(value="/{statementId}", method=RequestMethod.GET, produces="application/json")
+    public String getStatement(@PathVariable String statementId) {
+        return "{\"GET statementId\": \"" + statementId + "\"}";
+    }
+
+    /**
+     * Post a statement
+     * 
+     * @return JSON string of the statement object with the specified ID
+     */
+    @RequestMapping(value={"", "/"}, method=RequestMethod.POST, consumes="application/json", produces="application/json")
+    public String postStatement(@RequestBody String requestBody) {
+        return "{\"POST request body\": " + requestBody + "}";
+    }
+
+}
---
+++ src/test/java/org/apereo/openlrs/controllers/StatementControllerTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2014 Unicon (R) Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+package org.apereo.openlrs.controllers;
+
+import org.apereo.openlrs.Application;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 
+ * @author Robert E. Long (rlong @ unicon.net)
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes=Application.class)
+public class StatementControllerTest {
+
+    @Autowired
+    private StatementController statementController;
+
+    @Test
+    public void getAllStatementsTest() {
+        String response = statementController.getAllStatements();
+        Assert.assertNotNull(response);
+    }
+
+    @Test
+    public void getSingleStatementTest() {
+        String statementId = "1234-TEST-5678-UUID";
+        String response = statementController.getStatement(statementId);
+        Assert.assertNotNull(response);
+    }
+
+    @Test
+    public void postStatementTest() {
+        String requestBody = "{\"id\":\"UNIQUE_UUID\",\"actor\":\"actor1\",\"verb\":\"read\",\"object\":\"Little House on the Prairie\"}";
+        String response = statementController.getStatement(requestBody);
+        Assert.assertNotNull(response);
+    }
+
+}
