614499e0836e53caeaa346c805d15d2e8b9461d4
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-10-29 17:14:23 -0400

    TLCALIPER-17 - Added methods for getting a single event by ID and about page.  Added filter arguments to other GET handler.

--- src/main/java/org/apereo/openlrs/controllers/caliper/CaliperController.java
+++ src/main/java/org/apereo/openlrs/controllers/caliper/CaliperController.java
@@ -18,25 +18,27 @@ package org.apereo.openlrs.controllers.caliper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apereo.openlrs.exceptions.NotFoundException;
 import org.apereo.openlrs.exceptions.xapi.InvalidXAPIRequestException;
 import org.apereo.openlrs.model.caliper.CaliperEvent;
+import org.apereo.openlrs.model.xapi.About;
 import org.apereo.openlrs.services.caliper.CaliperService;
+import org.apereo.openlrs.utils.StatementUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 
 /**
  * @author ggilbert
- * @author Lance E Sloan <lsloan at umich dot edu>
+ * @author Lance E Sloan (lsloan at umich dot edu)
  */
 @RestController
 @RequestMapping("/caliper")
@@ -46,20 +48,58 @@ public class CaliperController {
     private ObjectMapper objectMapper;
     @Autowired
     private CaliperService caliperService;
+    @Value("${caliper.version}")
+    private String version;
 
-    // TODO: Add support for GET with ID or other filters
+
+    /**
+     *
+     * @param statementId ID of the statement to find
+     * @param allRequestParams other search parameters
+     * @return JSON node of requested Caliper event
+     */
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET, produces = "application/json;charset=utf-8", params = "statementId")
+    public JsonNode getByIdHandler(
+            @RequestParam(value = "statementId", required = true) String statementId,
+            @RequestParam Map<String, String> allRequestParams) {
+        logger.debug("Statement GET request received with parameters: {}",
+                allRequestParams);
+        JsonNode result = null;
+        try {
+            result = caliperService.getJsonNode(statementId);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new InvalidXAPIRequestException(e.getMessage(), e);
+        }
+
+        if (result == null) {
+            throw new NotFoundException("Statement for ID [" + statementId
+                    + "] not found.");
+        }
+        return result;
+    }
 
     /**
      * Get statement objects for the specified criteria
      *
+     * @param activity
+     * @param actor
+     * @param limit
+     * @param since
+     * @param until
      * @return JSON string of the statement objects matching the specified filter
      */
-    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
-    public List<JsonNode> getStatement() {
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET, produces = "application/json;charset=utf-8", params = "!statementId")
+    public List<JsonNode> getByFiltersHandler(
+            @RequestParam(value = "actor", required = false) String actor,
+            @RequestParam(value = "activity", required = false) String activity,
+            @RequestParam(value = "since", required = false) String since,
+            @RequestParam(value = "until", required = false) String until,
+            @RequestParam(value = "limit", required = false) String limit
+    ) {
         try {
             Map<String, String> filterMap = null;
-            // TODO: enable the following after this method has argument support
-//            filterMap = StatementUtils.createStatementFilterMap(actor, activity, since, until, limit);
+            filterMap = StatementUtils.createStatementFilterMap(actor, activity, since, until, limit);
             return caliperService.getJsonNodes(filterMap);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
@@ -67,14 +107,21 @@ public class CaliperController {
         }
     }
 
-    @RequestMapping(value = {"", "/"}, method = RequestMethod.POST)
-    public String event(@RequestBody String json) throws JsonProcessingException, IOException {
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.POST, consumes = "application/json", produces = "application/json;charset=utf-8")
+    public List<String> postHandler(@RequestBody String json) throws JsonProcessingException, IOException {
         logger.debug(json);
 
         JsonNode jsonNode = objectMapper.readTree(json);
         logger.debug("Type: " + jsonNode.findValue("@type").textValue());
         CaliperEvent caliperEvent = new CaliperEvent(jsonNode);
         caliperService.post(null, caliperEvent);
-        return caliperEvent.getKey();
+        List<String> caliperEventIds = new ArrayList<>();
+        caliperEventIds.add(caliperEvent.getKey());
+        return caliperEventIds;
+    }
+
+    @RequestMapping(value = "about", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+    public About about() {
+        return new About(version);
     }
 }
