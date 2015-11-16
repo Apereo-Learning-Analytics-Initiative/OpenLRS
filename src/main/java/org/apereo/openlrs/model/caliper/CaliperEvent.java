4828e0db9bd2f8e295861e247bd6feaf2885750b
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-11-16 16:07:03 -0500

    Generate an ID if the JSON doesn't have one, using a temporary property in the JSON to hold the value.

--- src/main/java/org/apereo/openlrs/model/caliper/CaliperEvent.java
+++ src/main/java/org/apereo/openlrs/model/caliper/CaliperEvent.java
@@ -15,75 +15,82 @@
  */
 package org.apereo.openlrs.model.caliper;
 
-import java.util.UUID;
-
-import org.apache.log4j.Logger;
-import org.apereo.openlrs.model.OpenLRSEntity;
-import org.imsglobal.caliper.events.Event;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.log4j.Logger;
+import org.apereo.openlrs.model.OpenLRSEntity;
+import org.imsglobal.caliper.events.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
 
 /**
  * @author ggilbert
- *
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CaliperEvent implements OpenLRSEntity {
 
-  private static final long serialVersionUID = 1L;
-  private Logger log = Logger.getLogger(CaliperEvent.class);
-  @JsonIgnore
-  public static final String OBJECT_KEY = "CALIPEREVENT";
-  
-  public CaliperEvent() {
-    
-  }
-  
-  public CaliperEvent(JsonNode jsonNode) {
-    this.json = jsonNode.toString();
-    this.key = UUID.randomUUID().toString();
-    this.event = CaliperUtils.toEvent(jsonNode);
-    this.type = CaliperUtils.getType(jsonNode);
-  }
+    @JsonIgnore
+    public static final String OBJECT_KEY = "CALIPEREVENT";
+    private static final long serialVersionUID = 1L;
+    @Autowired
+    private ObjectMapper objectMapper;
+    private Logger log = Logger.getLogger(CaliperEvent.class);
+    private String key;
+    private Event event;
+    private String type;
+    private String json;
 
-  private String key;
-  private Event event;
-  private String type;
-  private String json;
+    public CaliperEvent() {
+    }
 
-  @JsonIgnore
-  public Event getEvent() {
-    return event;
-  }
+    public CaliperEvent(JsonNode jsonNode) {
+        // this section: hack to keep ID around for later (needed for tier-two storage)
+        this.key = jsonNode.path("openlrsSourceId").asText();
+        if ((this.key == null) || (this.key.isEmpty())) {
+            this.key = UUID.randomUUID().toString();
+            ((ObjectNode) jsonNode).put("openlrsSourceId", this.key);
+        }
 
-  @JsonIgnore
-  public String getType() {
-    return type;
-  }
+        this.json = jsonNode.toString();
+        this.event = CaliperUtils.toEvent(jsonNode);
+        this.type = CaliperUtils.getType(jsonNode);
+    }
 
-  @Override
-  @JsonIgnore
-  public String getKey() {
-    return key;
-  }
+    @JsonIgnore
+    public Event getEvent() {
+        return event;
+    }
 
-  @Override
-  @JsonIgnore
-  public String getObjectKey() {
-    return OBJECT_KEY;
-  }
+    @JsonIgnore
+    public String getType() {
+        return type;
+    }
 
-  @JsonIgnore
-  public String toJSON() {
-    return this.json;
-  }
+    @Override
+    @JsonIgnore
+    public String getKey() {
+        return key;
+    }
 
-  @Override
-  public String toString() {
-    return "CaliperEvent [getKey()="
-        + getKey() + ", getObjectKey()=" + getObjectKey() + "]";
-  }
+    @Override
+    @JsonIgnore
+    public String getObjectKey() {
+        return OBJECT_KEY;
+    }
+
+    @JsonIgnore
+    public String toJSON() {
+        return this.json;
+    }
+
+    @Override
+    public String toString() {
+        return "CaliperEvent [getKey()="
+                + getKey() + ", getObjectKey()=" + getObjectKey() + "]";
+    }
 
 }
