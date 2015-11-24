bc8959578d8e860ac3dae4a7af6ffa7500eb1341
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2015-11-24 14:10:17 -0500

    TLCALIPER-17 - Based on code reviews by @bkirschn and @arwhyte: Removing unused parameters in getByIdHandler and switching to more general exceptions.

--- src/main/java/org/apereo/openlrs/controllers/caliper/CaliperController.java
+++ src/main/java/org/apereo/openlrs/controllers/caliper/CaliperController.java
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.apereo.openlrs.exceptions.NotFoundException;
 import org.apereo.openlrs.exceptions.caliper.InvalidCaliperFormatException;
-import org.apereo.openlrs.exceptions.xapi.InvalidXAPIRequestException;
+import org.apereo.openlrs.exceptions.InvalidRequestException;
 import org.apereo.openlrs.model.caliper.CaliperEvent;
 import org.apereo.openlrs.model.xapi.About;
 import org.apereo.openlrs.services.caliper.CaliperService;
@@ -56,28 +56,28 @@ public class CaliperController {
 
     /**
      * @param statementId      ID of the statement to find
-     * @param allRequestParams other search parameters
      * @return JSON node of requested Caliper event
      */
     @RequestMapping(value = {"", "/"}, method = RequestMethod.GET,
             produces = "application/json;charset=utf-8", params = "statementId")
-    public JsonNode getByIdHandler(
-            @RequestParam(value = "statementId", required = true) String statementId,
-            @RequestParam Map<String, String> allRequestParams) {
-        logger.debug("Statement GET request received with parameters: {}",
-                allRequestParams);
+    public JsonNode getByIdHandler (
+            @RequestParam(value = "statementId", required = true) String statementId
+    ) throws InvalidRequestException {
+
         JsonNode result = null;
+
         try {
             result = caliperService.getJsonNode(statementId);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
-            throw new InvalidXAPIRequestException(e.getMessage(), e);
+            throw new InvalidRequestException(e.getMessage(), e);
         }
 
         if (result == null) {
             throw new NotFoundException("Statement for ID [" + statementId
                     + "] not found.");
         }
+
         return result;
     }
 
@@ -93,20 +93,20 @@ public class CaliperController {
      */
     @RequestMapping(value = {"", "/"}, method = RequestMethod.GET,
             produces = "application/json;charset=utf-8", params = "!statementId")
-    public List<JsonNode> getByFiltersHandler(
+    public List<JsonNode> getByFiltersHandler (
             @RequestParam(value = "actor", required = false) String actor,
             @RequestParam(value = "activity", required = false) String activity,
             @RequestParam(value = "since", required = false) String since,
             @RequestParam(value = "until", required = false) String until,
             @RequestParam(value = "limit", required = false) String limit
-    ) {
+    ) throws InvalidRequestException {
         try {
             Map<String, String> filterMap = null;
             filterMap = StatementUtils.createStatementFilterMap(actor, activity, since, until, limit);
             return caliperService.getJsonNodes(filterMap);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
-            throw new InvalidXAPIRequestException(e.getMessage(), e);
+            throw new InvalidRequestException(e.getMessage(), e);
         }
     }
 
